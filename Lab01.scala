73749eb0961a4df290380745cb24b14a44972092
inceptez27 <101196410+inceptez27@users.noreply.github.com>
2022-03-10 07:23:22 +0530

    calling add method in main function

--- Lab01.scala
+++ Lab01.scala
@@ -3,6 +3,8 @@ object Lab01
    def main(args:Array[String])=
    {
        println("Scala calcualtor program")
+       val sum = add(10,20)
+       println("sum of a and b is " + sum)
    }
    
    def add(a:Int,b:Int) = a + b
